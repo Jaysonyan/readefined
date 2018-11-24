e3b029f7697d42db1061fd86bdc0169186568ce6
David Zhou <david.dz.zhou@gmail.com>
2018-11-24 16:11:02 -0500

    Add dynamic image sizing

--- buttonTest.py
+++ buttonTest.py
@@ -1,42 +1,43 @@
 import RPi.GPIO as GPIO
 from picamera import PiCamera
+from PIL import Image
 import time
 import os
-from PIL import Image
 
-sizeX = 1000
-sizeY = 1000
+imageStr = "image.jpg"
+
 
 def record():
     camera = PiCamera()
     camera.iso = 800
-    #camera.zoom = (0.25, 0.25, 0.5, 0.5)
+    # camera.zoom = (0.25, 0.25, 0.5, 0.5)
     camera.start_preview()
     while True:
-	time.sleep(2)
-        camera.capture("image.jpg")
-        img = Image.open("image.jpg")
-        img = img.crop(sizeX/4,sizeY/4,sizeX*3/4,sizeY*3/4)
-        crop.save("image.jpg")
+        time.sleep(2)
+        camera.capture(imageStr)
+        img = Image.open(imageStr)
+        sizeX = img.size[0]
+        sizeY = img.size[1]
+        img = img.crop(sizeX / 4, sizeY / 4, sizeX * 3 / 4, sizeY * 3 / 4)
+        img.save(imageStr)
         os.system("python master.py")
-	input_state = GPIO.input(18)
+        input_state = GPIO.input(18)
         if input_state == False:
             break
     camera.stop_preview()
 
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(23, GPIO.OUT)
 GPIO.output(23, GPIO.LOW)
 while True:
-           input_state = GPIO.input(18)
-           if input_state == False:
-               GPIO.output(23, GPIO.HIGH)
-               print("Button Pressed")
-               record()
-               break
-               
+    input_state = GPIO.input(18)
+    if input_state == False:
+        GPIO.output(23, GPIO.HIGH)
+        print("Button Pressed")
+        record()
+        break
+
 GPIO.cleanup()
-
-
--- master.py
+++ master.py
@@ -1,13 +1,19 @@
 import math
 import json
 import subprocess
+from PIL import Image
 from subprocess import Popen, PIPE
 import os
 
-centreX = 1920/2
-centreY = 1080/2
-process = subprocess.Popen(['gcloud', 'ml', 'vision', 'detect-text', 'image.jpg'], stdout=subprocess.PIPE)
-#process = subprocess.Popen(['gcloud', 'ml', 'vision', 'detect-text', 'image.jpg'], stdout=subprocess.PIPE)
+imageStr = 'image.jpg'
+image = Image.open(imageStr)
+centreX = image.size[0] / 2
+centreY = image.size[1] / 2
+
+process = subprocess.Popen(
+    ['gcloud', 'ml', 'vision', 'detect-text', imageStr], stdout=subprocess.PIPE)
+#process = subprocess.Popen(
+#     ['gcloud', 'ml', 'vision', 'detect-text', imageStr], stdout=subprocess.PIPE)
 
 wordsArr, err = process.communicate()
 wordsArr = json.loads(wordsArr)["responses"][0]["textAnnotations"][1:]
@@ -16,30 +22,32 @@ midWord = ""
 maxDist = 10000
 
 
-
 def getCenter(vertices):
     sumx = 0
     sumy = 0
     for vertex in vertices:
-        if ( (vertex.get("x") is None) or vertex.get("y") is None):
-          return [0,0]  
+        if (vertex.get("x") is None or vertex.get("y") is None):
+            return [0, 0]
         sumx += vertex["x"]
         sumy += vertex["y"]
     return [sumx/4.0, sumy/4.0]
 
+
 def getDist(curCenter):
-    val = math.sqrt( (centreX - curCenter[0] ) ** 2 + (centreY - curCenter[1] ) ** 2 )
+    val = math.sqrt((centreX - curCenter[0]) ** 2
+        + (centreY - curCenter[1]) ** 2)
     print val
     return val
 
+
 for words in wordsArr:
-  word = words["description"]
-  print words
-  print ""
-  curCenter = getCenter(words["boundingPoly"]["vertices"])
-  curDist = getDist(curCenter)
-  if(curDist < maxDist):
-      maxDist = curDist
-      midWord = word
+    word = words["description"]
+    print words
+    print ""
+    curCenter = getCenter(words["boundingPoly"]["vertices"])
+    curDist = getDist(curCenter)
+    if(curDist < maxDist):
+        maxDist = curDist
+        midWord = word
 print midWord
 os.system("say " + midWord)
