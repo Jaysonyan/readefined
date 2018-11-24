7ae97687965e60d904c41c60c723f6b25c9f42fc
Jayson Yan <jaysonyan1999@gmail.com>
2018-11-24 13:47:24 -0500

    Rename variables

--- master.py
+++ master.py
@@ -1,26 +1,25 @@
 import math
 import json
 import subprocess
-import Foundation
 from subprocess import Popen, PIPE
 import os
 
-centerx = 735/2
-centery = 1102/2
+centreX = 735/2
+centreY = 1102/2
 #process = subprocess.Popen(['../../google-cloud-sdk/bin/gcloud', 'ml', 'vision', 'detect-text', 'test.jpg'], stdout=subprocess.PIPE)
-process = subprocess.Popen(['gcloud', 'ml', 'vision', 'detect-text', './Documents/shot.jpg'], stdout=subprocess.PIPE)
+process = subprocess.Popen(['gcloud', 'ml', 'vision', 'detect-text', 'image.jpg'], stdout=subprocess.PIPE)
 
 wordsArr, err = process.communicate()
 wordsArr = json.loads(wordsArr)["responses"][0]["textAnnotations"][1:]
-#print wordsArr
+print wordsArr
 
 for words in wordsArr:
   word = words["description"]
   vertices = words["boundingPoly"]["vertices"]
-  if(vertices[0]["x"] < centerx and vertices[0]["y"] < centery):
-      if(vertices[1]["x"] > centerx and vertices[1]["y"] < centery):
-          if(vertices[2]["x"] > centerx and vertices[2]["y"] > centery):
-              if(vertices[3]["x"] < centerx and vertices[3]["y"] > centery):
+  if(vertices[0]["x"] < centreX and vertices[0]["y"] < centreY):
+      if(vertices[1]["x"] > centreX and vertices[1]["y"] < centreY):
+          if(vertices[2]["x"] > centreX and vertices[2]["y"] > centreY):
+              if(vertices[3]["x"] < centreX and vertices[3]["y"] > centreY):
                   print word
                   os.system("say " + word)
                   break
