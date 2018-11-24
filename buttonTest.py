d191a62cc473a2e30978bcc8e614de5b834afbcf
Jayson Yan <jaysonyan1999@gmail.com>
2018-11-24 12:52:54 -0500

    Add raspberry pi stuff

---
+++ buttonTest.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+from picamera import PiCamera
+import time
+
+
+def record():
+    camera = PiCamera()
+    camera.iso = 800
+    camera.start_preview()
+    while True:
+        camera.capture("image.jpg")
+        input_state = GPIO.input(18)
+        if input_state == False:
+            break
+        time.sleep(0.1)
+    camera.stop_preview()
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(23, GPIO.OUT)
+GPIO.output(23, GPIO.LOW)
+while True:
+           input_state = GPIO.input(18)
+           if input_state == False:
+               GPIO.output(23, GPIO.HIGH)
+               print("Button Pressed")
+               record()
+               break
+GPIO.cleanup()
+
+
