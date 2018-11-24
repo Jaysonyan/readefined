12febb3ec7a06723a7909f7b658ec2fbeb038989
eyhhuang <emily.huang1@hotmail.com>
2018-11-24 14:09:25 -0500

    Make new centre image calculation

--- master.py
+++ master.py
@@ -6,22 +6,34 @@ import os
 
 centreX = 2592/2
 centreY = 1944/2
-#process = subprocess.Popen(['../../google-cloud-sdk/bin/gcloud', 'ml', 'vision', 'detect-text', 'test.jpg'], stdout=subprocess.PIPE)
-process = subprocess.Popen(['gcloud', 'ml', 'vision', 'detect-text', 'image.jpg'], stdout=subprocess.PIPE)
+process = subprocess.Popen(['../../google-cloud-sdk/bin/gcloud', 'ml', 'vision', 'detect-text', 'test.jpg'], stdout=subprocess.PIPE)
+#process = subprocess.Popen(['gcloud', 'ml', 'vision', 'detect-text', 'image.jpg'], stdout=subprocess.PIPE)
 
 wordsArr, err = process.communicate()
 wordsArr = json.loads(wordsArr)["responses"][0]["textAnnotations"][1:]
-print wordsArr
+#print wordsArr
+midWord = ""
+maxDist = 10000
+
+
+
+def getCenter(vertices):
+    sumx = 0
+    sumy = 0
+    for vertex in vertices:
+        sumx += vertex["x"]
+        sumy += vertex["y"]
+    return [sumx/4.0, sumy/4.0]
+
+def getDist(curCenter):
+    return math.sqrt( (centreX - curCenter[0] ) ** 2 + (centreY - curCenter[1] ) ** 2 )
 
 for words in wordsArr:
   word = words["description"]
-  vertices = words["boundingPoly"]["vertices"]
-  if(vertices[0]["x"] < centreX and vertices[0]["y"] < centreY):
-      if(vertices[1]["x"] > centreX and vertices[1]["y"] < centreY):
-          if(vertices[2]["x"] > centreX and vertices[2]["y"] > centreY):
-              if(vertices[3]["x"] < centreX and vertices[3]["y"] > centreY):
-                  print word
-                  os.system("say " + word)
-                  break
-
-
+  curCenter = getCenter(words["boundingPoly"]["vertices"])
+  curDist = getDist(curCenter)
+  if(curDist < maxDist):
+      curDist = maxDist
+      midWord = word
+print midWord
+os.system("say " + midWord)
