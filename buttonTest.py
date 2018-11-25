9f29e33a336c0080a1f86d4fe61b9083adaa07b7
Jayson Yan <jaysonyan1999@gmail.com>
2018-11-25 03:09:07 -0500

    Fix threading bug

--- buttonTest.py
+++ buttonTest.py
@@ -1,49 +1,27 @@
-import os
-import time
 import RPi.GPIO as GPIO
-import threading
 from picamera import PiCamera
 from PIL import Image
+import time
+import os
 
+imageStr = "image.jpg"
 
-imgStr = "image.jpg"
-
-class callMasterThread (threading.Thread):
-   def __init__(self, threadID, name, counter):
-      threading.Thread.__init__(self)
-      self.threadID = threadID
-      self.name = name
-      self.counter = counter
-   def run(self):
-      os.system("python master.py")
 
 def record():
     camera = PiCamera()
     camera.iso = 800
     # camera.zoom = (0.25, 0.25, 0.5, 0.5)
     camera.start_preview()
-    master = myThread(1, "master-thread", 1)
-    led = myThread(2, "led-thread", 2)
-
     while True:
-        camera.capture(imgStr)
-        img = Image.open(imgStr)
+        #time.sleep(2)
+        camera.capture(imageStr)
+        img = Image.open(imageStr)
         sizeX = img.size[0]
         sizeY = img.size[1]
         img = img.crop((sizeX / 4, sizeY / 4, sizeX * 3 / 4, sizeY * 3 / 4))
-        img = img.rotate(180)
+	img = img.rotate(180)
         img.save(imageStr)
-
-        master.start()
-        ledOn = True
-        while threading.activeThreads() > 0 :
-            if ledOn:
-                GPIO.output(20, GPIO.LOW)
-            else:
-                GPIO.output(20, GPIO.HIGH)
-            ledOn = not ledOn
-            time.sleep(0.5)
-
+        os.system("python master.py")
         input_state = GPIO.input(26)
         if input_state == False:
             break
@@ -54,7 +32,6 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(21, GPIO.OUT)
-GPIO.setup(20, GPIO.OUT)
 GPIO.output(21, GPIO.LOW)
 while True:
     input_state = GPIO.input(26)
@@ -65,3 +42,4 @@ while True:
         break
 
 GPIO.cleanup()
+
